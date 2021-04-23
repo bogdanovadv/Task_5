d3bb83404bc41842d1228264a2d9bc6f94c7fa58
bogdanovadv <80579432+bogdanovadv@users.noreply.github.com>
2021-04-23 22:48:45 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,10 @@
-import sqlite3, telebot, requests
+import telebot, requests
 from telebot import types
-from newsapi import NewsApiClient
 
-telebot_token = "1794433387:AAGsLnL2AYlbBOoI5SFTuqqzU5ltMQ99P2U"
+telebot_token = ""
+host = 'http://127.0.0.1:8080'
+
 category = {'business', 'entertainment', 'general', 'health', 'science', 'sports', 'technology'}
-
 
 bot = telebot.TeleBot(telebot_token, parse_mode=None)
 
@@ -19,7 +19,7 @@ def main():
 @bot.message_handler(commands=['start'])
 def start(message):
     user_name = message.from_user.username
-    if requests.post(url='http://127.0.0.1:5000/users', data={'id':message.from_user.id}):
+    if requests.post(url=host + '/users', data={'id':message.from_user.id}):
         bot.send_message(message.chat.id, 'Привет, ' + str(user_name), reply_markup=main())
 
 @bot.message_handler(commands=['help'])
@@ -48,7 +48,7 @@ def cont(message):
 
     elif message.text == 'Посмотреть подписку' or message.text == 'Удалить подписку':
         keyboard = telebot.types.ReplyKeyboardMarkup(True)
-        news = requests.get(url='http://127.0.0.1:5000/subscriptions/categories', params={'id':message.from_user.id}).json()
+        news = requests.get(url=host + '/subscriptions/categories', params={'id':message.from_user.id}).json()
         for x in news:
             if message.text.find("Удалить") == 0:
                 x = "Удалить " + x
@@ -61,7 +61,7 @@ def cont(message):
 
     elif message.text == 'Посмотреть подборку' or message.text == 'Удалить ключевое слово':
         keyboard = telebot.types.ReplyKeyboardMarkup(True)
-        news = requests.get(url='http://127.0.0.1:5000/subscriptions/keywords', params={'id':message.from_user.id}).json()
+        news = requests.get(url=host + '/subscriptions/keywords', params={'id':message.from_user.id}).json()
         for x in news:
             if message.text.find("Удалить") == 0:
                 x = "Удалить " + x
@@ -72,28 +72,28 @@ def cont(message):
             bot.send_message(message.chat.id, "Выберите подборку", reply_markup=keyboard)
 
     elif message.text in category:
-        news = requests.get(url='http://127.0.0.1:5000/news', params={'category':message.text}).json()
+        news = requests.get(url=host + '/news', params={'category':message.text}).json()
         if news['totalResults'] == 0:
             bot.send_message(message.chat.id, "Новостей нет", reply_markup=main())
         for x in news["articles"][:10]:
             bot.send_message(message.chat.id, x["url"], reply_markup=main())
 
-    elif message.text in requests.get(url='http://127.0.0.1:5000/subscriptions/keywords', params={'id':message.from_user.id}).json():
-        news = requests.get(url='http://127.0.0.1:5000/news', params={'keyword':message.text}).json()
+    elif message.text in requests.get(url=host + '/subscriptions/keywords', params={'id':message.from_user.id}).json():
+        news = requests.get(url=host + '/news', params={'keyword':message.text}).json()
         if news['totalResults'] == 0:
             bot.send_message(message.chat.id, "Новостей нет", reply_markup=main())
         for x in news["articles"]:
             bot.send_message(message.chat.id, x["url"], reply_markup=main())
 
     elif message.text.find("Удалить") == 0:
-        if message.text.split()[1] in requests.get(url='http://127.0.0.1:5000/subscriptions/categories', params={'id':message.from_user.id}).json():
-            if requests.delete(url='http://127.0.0.1:5000/subscriptions/categories', data={'id':message.from_user.id, 'category':message.text.split()[1]}):
+        if message.text.split()[1] in requests.get(url=host + '/subscriptions/categories', params={'id':message.from_user.id}).json():
+            if requests.delete(url=host + '/subscriptions/categories', data={'id':message.from_user.id, 'category':message.text.split()[1]}):
                 bot.send_message(message.chat.id, "Подписка удалена", reply_markup=main())
             else:
                 bot.send_message(message.chat.id, 'Ошибка', reply_markup=main())
 
-        if message.text.split()[1] in requests.get(url='http://127.0.0.1:5000/subscriptions/keywords', params={'id':message.from_user.id}).json():
-            if requests.delete(url='http://127.0.0.1:5000/subscriptions/keywords', data={'id':message.from_user.id, 'keyword':message.text.split()[1]}):
+        if message.text.split()[1] in requests.get(url=host + '/subscriptions/keywords', params={'id':message.from_user.id}).json():
+            if requests.delete(url=host + '/subscriptions/keywords', data={'id':message.from_user.id, 'keyword':message.text.split()[1]}):
                 bot.send_message(message.chat.id, "Подписка удалена", reply_markup=main())
             else:
                 bot.send_message(message.chat.id, 'Ошибка', reply_markup=main())
@@ -101,20 +101,20 @@ def cont(message):
     elif message.text == 'Добавить подписку':
         keyboard = telebot.types.ReplyKeyboardMarkup(True)
         for x in category:
-            if not x in requests.get(url='http://127.0.0.1:5000/subscriptions/categories', params={'id':message.from_user.id}).json():
+            if not x in requests.get(url=host + '/subscriptions/categories', params={'id':message.from_user.id}).json():
                 keyboard.row("Добавить " + x)
         bot.send_message(message.chat.id, "Выберите подписку", reply_markup=keyboard)
 
     elif message.text.find("Добавить") == 0:
         if message.text.split()[1] in category:
-            if requests.post(url='http://127.0.0.1:5000/subscriptions/categories', data={'id':message.from_user.id, 'category':message.text.split()[1]}):
+            if requests.post(url=host + '/subscriptions/categories', data={'id':message.from_user.id, 'category':message.text.split()[1]}):
                 bot.send_message(message.chat.id, "Подписка добавлена", reply_markup=main())
             else:
                 bot.send_message(message.chat.id, 'Ошибка', reply_markup=main())
+                
     elif flag:
-        if not message.text in requests.get(url='http://127.0.0.1:5000/subscriptions/categories', params={'id':message.from_user.id}).json():
-            print(message.text)
-            if requests.post(url='http://127.0.0.1:5000/subscriptions/keywords', data={'id':message.from_user.id, 'keyword':message.text}):
+        if not message.text in requests.get(url=host + '/subscriptions/categories', params={'id':message.from_user.id}).json():
+            if requests.post(url=host + '/subscriptions/keywords', data={'id':message.from_user.id, 'keyword':message.text}):
                 bot.send_message(message.chat.id, "Ключевое слово добавлено", reply_markup=main())
             else:
                 bot.send_message(message.chat.id, 'Ошибка', reply_markup=main())
